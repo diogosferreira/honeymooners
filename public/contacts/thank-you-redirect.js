bc9ba835977141a1ec3a75baaa1c6e85e7cc70e1
diogosferreira <diogosferreira@users.noreply.github.com>
2025-11-24 18:58:03 +0000

    send

--- public/contacts/thank-you-redirect.js
+++ public/contacts/thank-you-redirect.js
@@ -8,7 +8,7 @@ export function thankYouRedirect() {
 
         // Run only on the base /thank-you (no lang folder)
         const lang = sessionStorage.getItem("site_lang") || "en";
-        console.log("lang");
+        console.log("lang " + lang);
         //window.location.href = `/${lang}/thank-you`;
 
 
